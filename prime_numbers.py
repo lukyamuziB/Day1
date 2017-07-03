347c450d4e1581574cd1ae90c171dca674ad6dc4
= <=>
2017-07-03 17:04:46 +0300

    better readable version of the code

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,17 +1,21 @@
 
-def prime(a):
-	if not isinstance(a, (int,float )):
+def prime(limit):
+	if not isinstance(limit, (int,float )):
 		raise TypeError("the limit has to be a number")
-	if a < 2:
+	if limit <= 2:
 		raise ValueError("Sorry, the lowest prime number is 2")
 	result = []
-	if a is 2:
+	if limit is 2:
 		return [a]
 
-	#since the prime even number is 2, we only look at odd numbers
-	for number in range(3,a,2):
-		if a < 9:
+	#since the only prime even number is 2, we only look at odd numbers
+	for number in range(3,limit,2):
+		if limit < 9:
 			result.append(number)
-		if ((a//2 != 0)and(a//3 != 0) and (a//5 != 0) and (a//5 != 0) and (a//7 != 0)):
+		if ((limit//2 != 0)and(limit//3 != 0) and (limit//5 != 0) and (limit//5 != 0) and (limit//7 != 0)):
 			result.append(number)
 	return result
+
+
+
+
